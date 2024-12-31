fa79f8aaf4347d1b7e9d09924687c65dd1eb17bc
AnthonyDavis009 <anthonybdavis.inbox@gmail.com>
2024-12-30 23:57:32 -0500

    Initial Commit

--- CodingTrackerAttemptYes/Program.cs
+++ CodingTrackerAttemptYes/Program.cs
@@ -1,10 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello and welcome, C# Academy! Another modification.");
-Console.WriteLine("Here I am.");
-Console.WriteLine("Another day.");
-Console.WriteLine("Hello and welcome, C# Academy! Another modification.");
-Console.WriteLine("Here I am.");
-Console.WriteLine("Another day.");
-Console.WriteLine("Hello and welcome, C# Academy! Another modification.");
-Console.WriteLine("Here I am.");
-Console.WriteLine("Another day.");
+void MyFirstMethod()
+{
+    // Do something
+}
+
+void MySecondMethod()
+{
+    // Do Something
+}
+
+void MyThirdMethod()
+{
+    // Do Something
+}
