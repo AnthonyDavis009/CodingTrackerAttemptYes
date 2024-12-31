2cbe37ff334825549d26a78f30c3da4370487978
AnthonyDavis009 <anthonybdavis.inbox@gmail.com>
2024-12-30 19:02:03 -0500

    More WriteLine

--- CodingTrackerAttemptYes/Program.cs
+++ CodingTrackerAttemptYes/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello and welcome, C# Academy!");
+Console.WriteLine("Here I am.");
+Console.WriteLine("Another day.");
