71c85fe33e2b15a0a934ef127ad151e4d58b814b
AnthonyDavis009 <158140286+AnthonyDavis009@users.noreply.github.com>
2024-12-30 23:24:42 -0500

    Keeping one message

--- CodingTrackerAttemptYes/Program.cs
+++ CodingTrackerAttemptYes/Program.cs
@@ -1,10 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello and welcome, C# Academy! Another modification.");
-Console.WriteLine("Here I am.");
-Console.WriteLine("Another day.");
-Console.WriteLine("Hello and welcome, C# Academy! Another modification.");
-Console.WriteLine("Here I am.");
-Console.WriteLine("Another day.");
-Console.WriteLine("Hello and welcome, C# Academy! Another modification.");
-Console.WriteLine("Here I am.");
-Console.WriteLine("Another day.");
